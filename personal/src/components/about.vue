dea09c6371796db73b634edd43b24caac578e9e0
xiangtianyu <2233919407@qq.com>
2018-02-22 21:55:18 +0800

    debug

---
+++ personal/src/components/about.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <span>
+      前端页面开发使用vue作为开发框架，使用element ui作为前端组件
+    </span><br>
+      <span>
+        项目github地址：<a href="https://github.com/xiangtianyu/personal" target="_blank">https://github.com/xiangtianyu/personal</a>
+    </span>
+  </div>
+</template>
